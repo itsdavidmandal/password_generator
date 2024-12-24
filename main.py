8591e5fef3790036df19a6e3926cf1631d6e410f
David Ramovich Mandal <davidramovichmandal@gmail.com>
2024-12-25 01:15:19 +0545

    first version

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import random
+
 upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-lower_case=upper_case.lower()
+lower_case = upper_case.lower()
 digits ="0123456789"
 symbols="!@#$%^&*()_+[]:;<>,./?"
 
+upper, lower, nums, syms = True, True, True, True
+
+all =""
+
+if upper:
+    all+=upper_case
+
+if lower:
+    all+=lower_case
+
+if nums:
+    all+=digits
+
+if syms:
+    all+=symbols
+
+length = 20 
+amount = 10 
+
+for x in range (amount):
+    password ="".join(random.sample(all, length))
+    print(password)
